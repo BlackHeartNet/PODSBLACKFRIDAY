79f49cf485332b65d84d5ea63dd9d6a8a3a7eef4
BlackHeartNet <juniormarketingfc@gmail.com>
2024-09-27 18:30:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const unitPrice = 39.99;
+let currentImageIndex = 1;
 
 function updatePrice() {
     const quantity = document.getElementById('quantity').value;
@@ -10,4 +11,12 @@ function buyProduct() {
     const quantity = document.getElementById('quantity').value;
     const flavor = document.getElementById('flavor').value;
     alert(`Você comprou ${quantity} Pod(s) Ignite V50 de sabor ${flavor}!`);
+}
+
+function nextImage() {
+    currentImageIndex++;
+    if (currentImageIndex > 9) {
+        currentImageIndex = 1; // Reinicia o índice após a última imagem
+    }
+    document.getElementById('productImage').src = `imagepod${currentImageIndex}.jpeg`;
 }
