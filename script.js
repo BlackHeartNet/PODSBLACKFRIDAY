054fce2544fd5ea26809b8396cf1421519193b59
BlackHeartNet <juniormarketingfc@gmail.com>
2024-09-27 17:28:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 let currentImage = 1;
+const totalImages = 2;
 
 function toggleImage() {
-    const imgElement = document.getElementById('podImage');
-    currentImage = currentImage === 1 ? 2 : 1;
-    imgElement.src = currentImage === 1 ? 'imagepod1.jpeg' : 'imagepod2.jpeg';
+    const imgElement1 = document.getElementById('podImage');
+    const imgElement2 = document.getElementById('podImage2');
+    
+    if (currentImage === 1) {
+        imgElement1.style.display = 'none';
+        imgElement2.style.display = 'block';
+        currentImage = 2;
+    } else {
+        imgElement1.style.display = 'block';
+        imgElement2.style.display = 'none';
+        currentImage = 1;
+    }
 }
 
 setInterval(toggleImage, 3000); // Troca de imagem a cada 3 segundos
